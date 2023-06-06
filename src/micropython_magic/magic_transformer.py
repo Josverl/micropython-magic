bf75e53d376907708644c6c2cdcd81ad0e421d8e
Jos Verlinde <jos_verlinde@hotmail.com>
2023-06-06 19:22:06 +0200

    Enable static code for %%script tag as well
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- src/micropython_magic/magic_transformer.py
+++ src/micropython_magic/magic_transformer.py
@@ -7,9 +7,11 @@ which would otherwise confuse it, and cause it to be disabled.
 """
 import re
 
-re_comment_magic = r"#\s(%%(micropython|python|mypy))"
-"""Regex to match a comment magic, e.g. `# %%micropython`"""	
+re_comment_magic = r"#\s(%%(micropython|python|mypy|script))"
+"""Regex to match a comment magic, e.g. `# %%micropython`"""
 subst = "\\g<1>"
+
+
 def comment_magic_transformer(lines: list[str]):
     """
     Transforms a cell with a commented cell magic into a cell without the comment, but with the %%celmagic.
@@ -17,4 +19,4 @@ def comment_magic_transformer(lines: list[str]):
     if "%%" not in lines[0]:
         return lines
 
-    return [re.sub(re_comment_magic, subst, lines[0])] + lines[1:]  
+    return [re.sub(re_comment_magic, subst, lines[0])] + lines[1:]
