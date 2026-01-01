90feff65fe465cef02833856b419cbd07f22d01e
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-01-01 17:25:38 +0100

    Add workspace configuration and simplify test_samples.py by removing port checks
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -11,7 +11,6 @@ from testbook.client import CellExecutionError, TestbookNotebookClient
 
 from micropython_magic.interactive import TIMEOUT
 from micropython_magic.mpr import IPyRemoteBoard
-from micropython_magic.script_access import path_for_script
 
 # avoid RuntimeWarning: Proactor event loop does not implement add_reader
 if os.name == "nt":
@@ -28,32 +27,6 @@ folder = Path("samples")
 )
 def test_samples(notebook_name: Path):
     print(f"Executing notebook {notebook_name}")
-    # Check for correct port before running the test
-    if "_" in Path(notebook_name).stem:
-        port = Path(notebook_name).stem.split("_")[-1]
-        if port in ["samd", "rp2"]:
-            # check if a device with this port is connected
-            cmd = ["mpremote", "run", str(path_for_script("fw_info.py"))]
-            output = subprocess.check_output(cmd, timeout=TIMEOUT)
-
-            if f"'port': '{port}'," not in output.decode("utf-8"):
-                pytest.xfail(f"Test requires a {port} device connected")
-        if port in ["wokwi", "rfc2217"]:
-            remoteport = "rfc2217://localhost:4000"
-            cmd = [
-                "mpremote",
-                "connect",
-                remoteport,
-                "run",
-                str(path_for_script("fw_info.py")),
-            ]
-            try:
-                output = subprocess.check_output(cmd, timeout=TIMEOUT)
-                if "'family': 'micropython'" not in output.decode("utf-8"):
-                    pytest.xfail(f"Test requires a device connected via {remoteport}")
-            except (subprocess.TimeoutExpired, subprocess.CalledProcessError):
-                pytest.xfail(f"Test requires a device connected via {remoteport}")
-
     with testbook(notebook_name, execute=True, timeout=TIMEOUT) as tb:
         # if any of the cells raised an assertion error, this will fail the test
         assert tb.code_cells_executed > 0  # at least one cell executed
